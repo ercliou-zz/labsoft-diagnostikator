a544a8904610d29c449aa92c67b3c82a2c8484fd
nakamura.tiago@gmail.com <nakamura.tiago@gmail.com@92944a4a-b2e5-3a61-5c8a-ff1f1dc57ad6>
2011-08-22 01:07:24 +0000

    inserido metodo que implementa diagnostico

--- pcs2034-Diagnostikator/src/br/com/diagnostikator/dao/DiagnosticoDAO.java
+++ pcs2034-Diagnostikator/src/br/com/diagnostikator/dao/DiagnosticoDAO.java
@@ -1,8 +1,14 @@
 package br.com.diagnostikator.dao;
 
+import java.util.List;
+
 import br.com.diagnostikator.model.Diagnostico;
+import br.com.diagnostikator.model.Doenca;
+import br.com.diagnostikator.model.Sintoma;
 
 public interface DiagnosticoDAO {
+	
+	public List<Doenca> getDoencas(List<Sintoma> sintomas);
 
 	public void save(Diagnostico diagnostico);
 
--- pcs2034-Diagnostikator/src/br/com/diagnostikator/dao/implementation/hibernate/DiagnosticoDAOHibernate.java
+++ pcs2034-Diagnostikator/src/br/com/diagnostikator/dao/implementation/hibernate/DiagnosticoDAOHibernate.java
@@ -1,9 +1,15 @@
 package br.com.diagnostikator.dao.implementation.hibernate;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.hibernate.Session;
 
 import br.com.diagnostikator.dao.DiagnosticoDAO;
 import br.com.diagnostikator.model.Diagnostico;
+import br.com.diagnostikator.model.Doenca;
+import br.com.diagnostikator.model.Sintoma;
 
 public class DiagnosticoDAOHibernate implements DiagnosticoDAO {
 
@@ -35,5 +41,54 @@ public class DiagnosticoDAOHibernate implements DiagnosticoDAO {
 	public Diagnostico getByID(long id) {
 		return (Diagnostico) session.get(Diagnostico.class, id);
 	}
-	
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public List<Doenca> getDoencas(List<Sintoma> sintomas) {
+		ArrayList<Doenca> doencasObj = new ArrayList<Doenca>();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("(s.nome = '" + sintomas.get(0).getNome() + "'");
+
+		for (int i = 1; i < sintomas.size(); i++) {
+			sb.append(" OR s.nome = '" + sintomas.get(i).getNome() + "'");
+		}
+
+		sb.append(")");
+		List doencas = session
+				.createSQLQuery(
+						"SELECT d.id, d.descricao, d.nome FROM doenca d, sintoma s, doenca_sintoma ds WHERE d.id = ds.doenca_id AND s.id = ds.sintomas_id AND "
+								+ sb.toString() + " GROUP BY d.nome")
+				.addEntity("d", Doenca.class).list();
+
+		for (Iterator it = doencas.iterator(); it.hasNext();) {
+
+			doencasObj.add((Doenca) it.next());
+		}
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Doenca> tmp = (ArrayList<Doenca>) doencasObj.clone();
+
+		for (Doenca d : tmp) {
+			for (Sintoma s : sintomas) {
+				boolean temSintoma = false;
+				for (Sintoma sd : d.getSintomas()) {
+					if (sd.equals(s)) {
+						temSintoma = true;
+					}
+				}
+				if (temSintoma == false) {
+					doencasObj.remove(d);
+				}
+			}
+		}
+
+		/*
+		 * Lista as doencas encontradas for (Doenca d : doencasObj) {
+		 * System.out.println(d.getNome()); }
+		 */
+
+		return doencasObj;
+	}
+
 }
